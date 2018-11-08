10cc0af7262a74364895b9ce1c57d365096fbaef
Yoshi Ri <yoshiyoshidetteiu@gmail.com>
2018-11-09 00:20:54 +0900

    captured new videos and markerimages

--- trial/takevideo.py
+++ trial/takevideo.py
@@ -15,14 +15,18 @@ from vmarker import *
 
 if __name__=='__main__':
     cap = cv2.VideoCapture(0)
-
+    args = sys.argv
+    duration = 10000 # 10000 sample = 333s = 5.5m
+    if len(args) > 1:
+        duration = int(args[1])
     # Define the codec and create VideoWriter object
     fourcc = cv2.VideoWriter_fourcc(*'XVID')
     out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
 
     try:
-        while ~cap.isOpened():
+        while ~cap.isOpened() and (duration > 0 ):
             ret,frame = cap.read()
+            duration = duration - 1
             if ret==True:
 
                 # write the flipped frame
