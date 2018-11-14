44def54fba49c61c16b9900ab19305945eed9d75
YoshiRi <yoshiyoshidetteiu@gmail.com>
2018-11-14 11:27:22 +0900

    solve svd problem and added calculation

--- trial/Videos/SaveAndDetectPlotData.py
+++ trial/Videos/SaveAndDetectPlotData.py
@@ -46,33 +46,35 @@ if __name__=='__main__':
     mtv = np.mean(tvplot,axis=0)
     mrv = np.mean(rvplot,axis=0)
 
-    fig=plt.figure(figsize=(18, 16), dpi= 80, facecolor='w', edgecolor='k')
-    plt.subplot(231)
-    plt.plot(tvplot[:,0])
-    plt.plot([0,plen],[mtv[0], mtv[0]])
-    #plt.legend("tx data","mean")
-    plt.subplot(232)
-    plt.plot(tvplot[:,1])
-    plt.plot([0,plen],[mtv[1], mtv[1]])
-    #plt.legend("ty data","mean")
-    plt.subplot(233)
-    plt.plot(tvplot[:,2])
-    plt.plot([0,plen],[mtv[2], mtv[2]])
-    #plt.legend("tz data","mean")
-    plt.subplot(234)
-    plt.plot(rvplot[:,0])
-    plt.plot([0,plen],[mrv[0], mrv[0]])
-    #plt.legend("rx data","mean")
-    plt.subplot(235)
-    plt.plot(rvplot[:,1])
-    plt.plot([0,plen],[mrv[1], mrv[1]])
-    #plt.legend("ry data","mean")
-    plt.subplot(236)
-    plt.plot(rvplot[:,2])
-    plt.plot([0,plen],[mrv[2], mrv[2]])
-    #plt.legend("rz data","mean")
-    plt.ion()
-    plt.show()
+    success_flag = vm.hasCameraPose
+    if success_flag:
+        fig=plt.figure(figsize=(18, 16), dpi= 80, facecolor='w', edgecolor='k')
+        plt.subplot(231)
+        plt.plot(tvplot[:,0])
+        plt.plot([0,plen],[mtv[0], mtv[0]])
+        #plt.legend("tx data","mean")
+        plt.subplot(232)
+        plt.plot(tvplot[:,1])
+        plt.plot([0,plen],[mtv[1], mtv[1]])
+        #plt.legend("ty data","mean")
+        plt.subplot(233)
+        plt.plot(tvplot[:,2])
+        plt.plot([0,plen],[mtv[2], mtv[2]])
+        #plt.legend("tz data","mean")
+        plt.subplot(234)
+        plt.plot(rvplot[:,0])
+        plt.plot([0,plen],[mrv[0], mrv[0]])
+        #plt.legend("rx data","mean")
+        plt.subplot(235)
+        plt.plot(rvplot[:,1])
+        plt.plot([0,plen],[mrv[1], mrv[1]])
+        #plt.legend("ry data","mean")
+        plt.subplot(236)
+        plt.plot(rvplot[:,2])
+        plt.plot([0,plen],[mrv[2], mrv[2]])
+        #plt.legend("rz data","mean")
+        #plt.ion()
+        plt.show()
  
     # ture camera poses
     Rmean,_ = cv2.Rodrigues(np.float32(mrv))
@@ -92,15 +94,24 @@ if __name__=='__main__':
         sframe = frame.copy()
         #cv2.startWindowThread()
         mask,cpts,flag = extractRed(frame)
-        centerpts.append(cpts)
+        if flag:
+            centerpts.append(cpts)
 
     plt.figure(2)
     plt.plot(centerpts)
-    plt.ion()
+    #plt.ion()
     plt.show()
 
-    vm.rvecs = np.array(mrv)
-    vm.tvecs = np.array(mtv).reshape(3,1)
+
+    if success_flag:
+        vm.rvecs = np.array(mrv)
+        vm.tvecs = np.array(mtv).reshape(3,1)
+    else:
+        print("SET PRESET VALUE")
+        vm.rvecs = np.array([1.72836771043141, 1.2681418485393647, -0.6854996049482])
+        vm.tvecs = np.array([0.2619710716591172, 0.29167728232950163, 3.457404829379607]).reshape(3,1)
+        
+
     objpts = []
     for points in centerpts:
         objxy = vm.getobjpose_1(points,0.13)
@@ -112,6 +123,7 @@ if __name__=='__main__':
     mobjpts = np.mean(objpts,axis=0)
     print(mobjpts)
     plt.show()
+
     # save result with yaml
     import yaml
     with open(capname+'.yml', 'w') as stream:
