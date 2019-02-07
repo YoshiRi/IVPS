d77f3f6ba7d3280c18236b5c825696ba75b4fdde
YoshiRi <yoshiyoshidetteiu@gmail.com>
2019-02-07 18:08:46 +0900

    specify opencv version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name = 'IVPS',
     version = '0.1',
     description='This is test codes for travis ci',
-    install_requires=['numpy','opencv-contrib-python','opencv-python','docopt','matplotlib'],
+    install_requires=['numpy','opencv-contrib-python==3.4.4.19','opencv-python==3.4.4.19','docopt','matplotlib'],
     packages = find_packages(exclude=('sample', 'markers','trial')),
     license = 'MIT',
     author='Yoshi Ri',
