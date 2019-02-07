2831604fb44aa0e1290bca73ecbbb1758273d8a1
YoshiRi <yoshiyoshidetteiu@gmail.com>
2019-02-07 15:41:33 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
 from setuptools import setup, find_packages
 import sys
 
-#sys.path.append('./src')
-sys.path.append('./sample')
+sys.path.append('./calib_usb')
 
 setup(
     name = 'IVPS',
     version = '0.1',
     description='This is test codes for travis ci',
-    packages = find_packages(),
+    install_requires=['numpy','opencv-contrib-python','opencv-contrib','docopt','matplotlib'],
+    packages = find_packages(exclude=('sample', 'markers','trial')),
+    license = 'MIT',
+    author='Yoshi Ri',
     test_suite = 'IVPS_test.suite'
 )
