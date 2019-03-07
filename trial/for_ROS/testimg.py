d592bee17229f3c9496fa746eaa715cb53b546a4
Yoshi Ri <yoshiyoshidetteiu@gmail.com>
2019-03-07 16:21:33 +0900

    added test program

--- trial/for_ROS/testimg.py
+++ trial/for_ROS/testimg.py
@@ -17,7 +17,7 @@ img = cv2.imread("limage.png")
 K = np.loadtxt("vm_K.csv",delimiter=",").reshape(3,3)
 dist_coef = np.loadtxt('vm_D.csv',delimiter=",")
 vm = vmarker(K=K,dist=dist_coef,markerpos_file="roomA_ground_orig.csv")
-vm.getcamerapose(img.copy())
+tc_g = vm.getcamerapose(img.copy())
 
 
 # redtracker
@@ -26,12 +26,33 @@ tracker.track(img)
 pos=tracker.getpos()
 
 # see the mask
-mask,centers,flag,validnum = tracker.extractRed(img)
+#mask,centers,flag,validnum = tracker.extractRed(img)
+#cv2.imshow("Mask",mask)
 
-cv2.imshow("Mask",mask)
 depth = np.loadtxt('dimage.csv',delimiter=",")
-
 cv2.waitKey(0)
 
+import math
 print(pos)
-print(depth[int(pos[1]),int(pos[0])])
+print(depth[math.floor(pos[1]):math.floor(pos[1])+2,math.floor(pos[0]):math.floor(pos[0])+2])
+
+
+cv2.destroyAllWindows()
+
+# 3D pose on camera coordinate
+Z = depth[int(pos[1]),int(pos[0])]
+X = Z * pos[0]
+Y = Z * pos[1]
+
+tc = vm.tvecs.reshape(3,1)
+Rc,_ = cv2.Rodrigues(vm.rvecs)
+
+to_c = np.dot(np.linalg.inv(K),np.array([X,Y,Z]).reshape(3,1))
+
+print("Stereo Based Method")
+print(tc_g+np.dot(Rc.T,to_c))
+
+# proposed method 1
+
+print("Proposed Method")
+print(vm.getobjpose_1(pos,0.12))
